b35d891ad62db3bbddf6684a7dee8adce0ed9013
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-08-28 12:32:33 +0530

    Rollno

---
+++ cspp2 assignments/m1/Assignment-1/Solution.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+        String Name = s.nextLine();
+        long Rollno = s.nextLong();
+		System.out.println("Name : "+Name + "Roll Number :"+Rollno);
+	}
+}
