bb0478fef4935c5b9358162cc562114892d7175c
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-08-28 20:19:39 +0530

    bigger_equal_smaller

---
+++ cspp2 practice/m2/bigger_equal_smaller.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class bigger_equal_smaller{
+	public static void main(String[] args)
+	{
+	Scanner s = new Scanner(System.in);
+	int varA = s.nextInt();
+	int varB = s.nextInt();
+	if(varA>varB)
+	{
+	System.out.println("Bigger");
+	}
+	else if(varA==varB){
+	System.out.println("Equal");
+	}
+	else
+	{
+	System.out.println("smaller");
+	}
+
+	}
+
+	}
+	
