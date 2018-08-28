8337bf211de2f303258960fe27ccdfce38b7a37b
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-08-28 11:49:35 +0530

    quotientremainder

---
+++ cspp2 practice/m1/QuotientRemainder.java
@@ -0,0 +1,10 @@
+public class QuotientRemainder
+{
+	public static void main(String[] args)
+	{
+		int x=2,y=3,quo,rem;
+		quo = x/y;
+		rem = x%y;
+		System.out.println("rem: "+x+ " and " +"quo: "+y);
+	}
+}
