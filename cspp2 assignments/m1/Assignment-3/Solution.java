a6466818ce29386aad4f805e74176581f7e06410
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-08-28 20:12:12 +0530

    fahrenheittodegrees

---
+++ cspp2 assignments/m1/Assignment-3/Solution.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution {
+	/*
+	Do not modify this main function.
+	*/
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		double degreesCelsius = scan.nextDouble();
+		double calculateFromDegreesToFarenheit = degreesCelsius*1.8+32;
+		System.out.println(calculateFromDegreesToFarenheit);
+	}
+	/*
+	Need to fill the calculateFromDegreesToFarenheit function and print the output
+	of fahrenheit.
+	*/
+}
