1031affbf2f707a3f4a838f56b8e21a49d0b63c3
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-08-28 18:25:27 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cspp2 assignments/m2/Assignment-2/Solution.java
+++ cspp2 assignments/m2/Assignment-2/Solution.java
@@ -15,11 +15,9 @@ public class Solution {
 	static void rootsOfQuadraticEquation(int a, int b, int c){
      double root1= (-b + Math.sqrt( b*b - 4*a*c ) )/ (2*a);
      double root2= (-b - Math.sqrt( b*b - 4*a*c ) )/ (2*a);
-     System.out.println("The roots1 are: "+ root1);
-     System.out.println("The roots2 are: " + root2);
+     System.out.println("The roots1 and root2 are: "+ root1+ root2);
 	}
 	/*
-
 	Need to write the rootsOfQuadraticEquation function and print the output.
 	*/
 }
