d5c65ed6d7cc24a5c324b90dee9f1f67aec9a3c1
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-08-30 14:51:56 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2 assignments/m4/Assignment-1/Solution.java
+++ cspp2 assignments/m4/Assignment-1/Solution.java
@@ -8,11 +8,11 @@ public class Solution {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int a[] = new int[n];
-		for (int i = 0;i < n;i++) {
+		for (int i = 0; i < n; i++) {
 			a[i] = sc.nextInt();	
 		}
 		int max = a[0];
-		for (int i = 0; i < n;i++) {
+		for (int i = 0; i < n; i++) {
 			if (max < a[i]) {
 				max = a[i];
 			}
