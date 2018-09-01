db07a07519978243e623cacba770d55bf977c91f
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-09-01 12:58:04 +0530

    "check_style_score -> 1 of 6 passed. style score: 0/1 "

--- cspp2 assignments/m6/Assignment-1/Solution.java
+++ cspp2 assignments/m6/Assignment-1/Solution.java
@@ -22,7 +22,7 @@ public final class Solution {
     	int factor = 0;
 
 	 for (int i = 2;i < n;i++ ) {
-	 	if (n % i == 0 ) {
+	 	if (n%i== 0) {
 	 		factor = factor + 1;
 	 	}
 	 	if (factor > 2) {
