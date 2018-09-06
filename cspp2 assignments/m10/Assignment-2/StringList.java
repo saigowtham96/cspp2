1fb11e5ec1a6eaf7237b736cf4b59545916d3e61
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-09-06 20:04:18 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- cspp2 assignments/m10/Assignment-2/StringList.java
+++ cspp2 assignments/m10/Assignment-2/StringList.java
@@ -188,7 +188,7 @@ public class StringList implements StringListInterface{
             size--;
         }
         else {
-        System.out.println("Invalid position Exception");
+        System.out.println("Invalid Position Exception");
     }
         // write the logic for remove here.
         // Think about what to do to the size variable.
